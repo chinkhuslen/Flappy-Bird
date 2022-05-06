84f6cb9b46cd583cc114fa15374f654550221ba3
chinkhuslen <chinkhuslen@iMac-von-iMac-14.local>
2022-05-06 12:11:01 +0800

    jijig updates

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let c = document.getElementById("myCanvas");
+c.width = window.innerWidth/100*60
+c.height = window.innerHeight/100*70
 let ctx = c.getContext("2d");
 const HEIGHT = c.height;
 const dv = document.createElement("div");
@@ -14,28 +16,54 @@ let bird = {
   y: 20,
 };
 
+let pipe = []
+
 class Rectangle {
-  constructor(width, height) {
+  constructor(posX, posY, width, height) {
+    this.posX = posX;
+    this.posY = posY;
     this.width = width;
     this.height = height;
   }
 }
+
+
+class Pipe{
+  constructor(topRec, botRec){
+    this.top = topRec;
+    this.bot = botRec;
+  }
+}
+
+for(let i = 0;i < parseInt((parseInt(c.width/200+1)%2) ? c.width/200+1 : c.width/200);i++){
+  let rand = randomNumber();
+ let top = new Rectangle(200*i,rand,100,HEIGHT)
+ let bot = new Rectangle(200*i,rand-HEIGHT-150,100,HEIGHT)
+ let pipeN = new Pipe(top,bot);
+ pipe.push(pipeN)
+}
+for(let i = 0;i < parseInt((parseInt(c.width/200+1)%2) ? c.width/200+1 : c.width/200);i++){
+  console.log(pipe[i])
+}
+function updatePipe(i){
+  pipe[i].top.posY = pipe[i].bot.posY = randomNumber();
+  pipe[i].top.posX =pipe[i].bot.posX = c.width
+
+}
 let score = 0;
 let imge = document.getElementById('pics')
-let rect1 = new Rectangle(100, HEIGHT);
-let rect2 = new Rectangle(100, HEIGHT);
-let rectPosX=c.width,rectPosY = randomNumber(); 
+let rectPosX=0,rectPosY = randomNumber(); 
+let currentIndexOfPipe = 0
 var interval1,interval2;
 interval1 = setInterval(drawChar, 10);
 interval2 = setInterval(() => {
     if(bird.y<HEIGHT-20 && bird.y>0){
-        bird.y++;
+        // bird.y++;
     }
-    if(rectPosX>-150){
-        rectPosX--;
+    if(rectPosX<c.width){
+        rectPosX++;
     }else{
-        rectPosX = c.width
-        rectPosY = randomNumber();
+        rectPosX = 0
     }
     },10)
 
@@ -43,47 +71,54 @@ function randomNumber() {
   return Math.floor(Math.random() * (c.height - 350) + 250);
 } 
 function drawOb() {
-  ctx.beginPath();
-  ctx.rect(rectPosX, rectPosY, rect1.width, rect1.height);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.rect(rectPosX, rectPosY - HEIGHT - 150, rect2.width, rect2.height);
-  ctx.stroke();
+  for(let i of pipe){
+    ctx.beginPath();
+    ctx.rect(  i.bot.posX - rectPosX, i.bot.posY, 100, HEIGHT);
+    ctx.stroke();
+  
+    ctx.beginPath();
+    ctx.rect( i.top.posX - rectPosX, i.top.posY,100, HEIGHT);
+    ctx.stroke();
+  }
 }
 
 function drawChar() {
   ctx.clearRect(0, 0, c.width, c.height);
   ctx.drawImage(imge,80,80,500,400,bird.x-20,bird.y-20,40,40)
   drawOb();
-  if((bird.x + 20 >= rectPosX && bird.x <= rectPosX+100)&& (bird.y+20 > rectPosY || bird.y-20 < rectPosY-150)){
-      clearInterval(interval1)
-      clearInterval(interval2)
-      document.body.appendChild(dv)
-  }else{
-      if(bird.x == rectPosX+100){
-          score +=10;
-          document.getElementById("score").innerText = score;
-      }
-  }
+  // if((bird.x + 20 >= rectPosX && bird.x <= rectPosX+100)&& (bird.y+20 > rectPosY || bird.y-20 < rectPosY-150)){
+  //     clearInterval(interval1)
+  //     clearInterval(interval2)
+  //     document.body.appendChild(dv)
+  // }else{
+  //     if(bird.x == rectPosX+100){
+  //         score +=10;
+  //         document.getElementById("score").innerText = score;
+  //     }
+  // }
 }
 
 window.addEventListener("keypress", (event) => {
-  if (event.keyCode === 32) {
+  if(event.keyCode===32){
     if (bird.y > 30) {
       bird.y -= 30;
     }
   }
+});
+c.addEventListener("click", (event) => {
+    if (bird.y > 30) {
+      bird.y -= 30;
+    }
 });
 function restart(){
     dv.remove()
 interval1 = setInterval(drawChar, 10);
 interval2 = setInterval(() => {
     if(bird.y<HEIGHT-20 && bird.y>0){
-        bird.y++;
+        // bird.y++;
     }
     if(rectPosX>-150){
-        rectPosX--;
+        // rectPosX--;
     }else{
         rectPosX = c.width
         rectPosY = randomNumber();
